16699c583df5a75b14b6e26727fbc3f53b5d4384
SUDHIR BODDU <bsudhir6@gmail.com>
2022-10-18 08:20:23 +0530

    Removed Hard coding of values inside code. moved them to constants.

--- evernote-batch-app/src/main/java/in/net/sudhir/evernotebatchapp/EvernoteBatchAppApplication.java
+++ evernote-batch-app/src/main/java/in/net/sudhir/evernotebatchapp/EvernoteBatchAppApplication.java
@@ -2,6 +2,7 @@ package in.net.sudhir.evernotebatchapp;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import in.net.sudhir.evernotebatchapp.component.EvernoteAppComponent;
+import in.net.sudhir.evernotebatchapp.config.EvernoteBatchCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,17 @@ public class EvernoteBatchAppApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(EvernoteBatchAppApplication.class);
 
+    private EvernoteBatchCommand command;
+    private String notebookName;
+
+    private static final String populateDataFromEvernoteSwitch= "app.operations.populatedatafromevernote";
+    private static final String startFileLoadFtp= "app.operations.startfileloadftp";
+    private static final String evernoteInformation= "app.operations.evernoteinformation";
+    private static final String evernoteUploadTask= "app.operations.evernoteuploadtask";
+    private static final String startFtpDownloadTask= "app.operations.startftpdownloadtask";
+    private static final String evernoteDownloadTask= "app.operations.evernotedownloadtask";
+    private static final String evernoteDeleteTask= "app.operations.evernotedeletetask";
+
     public static void main(String[] args) {
         SpringApplication.run(EvernoteBatchAppApplication.class, args);
     }
@@ -33,42 +45,42 @@ public class EvernoteBatchAppApplication {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            if(args.length == 1){
-                if(args[0].equalsIgnoreCase("POPULATE_DATA_FROM_EVERNOTE")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.populatedatafromevernote")))
+            if(validateArgs(args)){
+                if(command == EvernoteBatchCommand.POPULATE_DATA_FROM_EVERNOTE){
+                    if(Boolean.parseBoolean(environment.getProperty(populateDataFromEvernoteSwitch)))
                         evernoteAppController.populateDatabase();
                     else
                         logger.error("Operation turned off.");
-                }else if(args[0].equalsIgnoreCase("EVERNOTE_INFORMATION")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.evernoteinformation")))
+                }else if(command == EvernoteBatchCommand.EVERNOTE_INFORMATION){
+                    if(Boolean.parseBoolean(environment.getProperty(evernoteInformation)))
                         evernoteAppController.evernoteInformation();
                     else
                         logger.error("Operation turned off.");
-                }else if(args[0].equalsIgnoreCase("START_FILE_LOAD_FTP")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.startfileloadftp")))
+                }else if(command == EvernoteBatchCommand.START_FILE_LOAD_FTP){
+                    if(Boolean.parseBoolean(environment.getProperty(startFileLoadFtp)))
                         evernoteAppController.initiateFileLoad();
                     else
                         logger.error("Operation turned off.");
-                }else if(args[0].equalsIgnoreCase("EVERNOTE_UPLOAD_TASK")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.evernoteuploadtask")))
+                }else if(command == EvernoteBatchCommand.EVERNOTE_UPLOAD_TASK){
+                    if(Boolean.parseBoolean(environment.getProperty(evernoteUploadTask)))
                         evernoteAppController.uploadToEvernote();
                     else
                         logger.error("Operation turned off.");
-                }else if(args[0].equalsIgnoreCase("START_FTP_DOWNLOAD_TASK")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.startftpdownloadtask")))
+                }else if(command == EvernoteBatchCommand.START_FTP_DOWNLOAD_TASK){
+                    if(Boolean.parseBoolean(environment.getProperty(startFtpDownloadTask)))
                         evernoteAppController.downloadToFTPLocation();
                     else
                         logger.error("Operation turned off.");
-                }
-            }else if(args.length == 2){
-                if(args[0].equalsIgnoreCase("EVERNOTE_DOWNLOAD_TASK")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.evernotedownloadtask")))
-                        evernoteAppController.downloadFromEvernote(args[1]);
+                }else if(command == EvernoteBatchCommand.EVERNOTE_DOWNLOAD_TASK){
+                    if(Boolean.parseBoolean(environment.getProperty(evernoteDownloadTask))){
+                        evernoteAppController.downloadFromEvernote(notebookName);
+                    }
                     else
                         logger.error("Operation turned off.");
-                } else if(args[0].equalsIgnoreCase("EVERNOTE_DELETE_TASK")){
-                    if(Boolean.parseBoolean(environment.getProperty("app.operations.evernotedeletetask")))
-                        evernoteAppController.deleteFromEvernote(args[1]);
+                } else if(command == EvernoteBatchCommand.EVERNOTE_DELETE_TASK){
+                    if(Boolean.parseBoolean(environment.getProperty(evernoteDeleteTask))) {
+                        evernoteAppController.deleteFromEvernote(notebookName);
+                    }
                     else
                         logger.error("Operation turned off.");
                 }
@@ -76,6 +88,18 @@ public class EvernoteBatchAppApplication {
         };
     }
 
+    private boolean validateArgs(String[] args) {
+        if(args.length > 0){
+            command = EvernoteBatchCommand.valueOf(args[0]);
+            if(command != null){
+                if((command == EvernoteBatchCommand.EVERNOTE_DELETE_TASK) || (command == EvernoteBatchCommand.EVERNOTE_DOWNLOAD_TASK)){
+                    notebookName = args[1];
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 }
---
+++ evernote-batch-app/src/main/java/in/net/sudhir/evernotebatchapp/config/EvernoteBatchCommand.java
@@ -0,0 +1,31 @@
+package in.net.sudhir.evernotebatchapp.config;
+
+/***
+ Package Name: in.net.sudhir.evernotebatchapp.config
+ User Name: SUDHIR
+ Created Date: 17-10-2022 at 16:54
+ Description:
+ */
+public enum EvernoteBatchCommand {
+
+
+    POPULATE_DATA_FROM_EVERNOTE("POPULATE_DATA_FROM_EVERNOTE"),
+    EVERNOTE_INFORMATION("EVERNOTE_INFORMATION"),
+    START_FILE_LOAD_FTP("START_FILE_LOAD_FTP"),
+    EVERNOTE_UPLOAD_TASK("EVERNOTE_UPLOAD_TASK"),
+    START_FTP_DOWNLOAD_TASK("START_FTP_DOWNLOAD_TASK"),
+    EVERNOTE_DOWNLOAD_TASK("EVERNOTE_DOWNLOAD_TASK"),
+    EVERNOTE_DELETE_TASK("EVERNOTE_DELETE_TASK")
+    ;
+
+    private final String command;
+
+    EvernoteBatchCommand(String command) {
+        this.command = command;
+    }
+
+    @Override
+    public String toString() {
+        return command;
+    }
+}
